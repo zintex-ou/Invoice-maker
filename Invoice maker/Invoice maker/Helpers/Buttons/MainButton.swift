47ed1012c40f455e030750a0f1a77eba3ce84753
Ilya Zablotski <ilya.zablotski@solveit.dev>
2025-06-10 16:13:46 +0300

    Setup MainButton

---
+++ Invoice maker/Invoice maker/Helpers/Buttons/MainButton.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+struct MainButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .font(.sans(style: .semiBold, size: 16))
+            .foregroundStyle(configuration.isPressed ? .white.opacity(0.5) : .white)
+            .frame(maxWidth: .infinity, maxHeight: 48)
+            .background(.violet4663FF)
+            .clipShape(Capsule())
+            .minimumScaleFactor(0.8)
+            .scaleEffect(configuration.isPressed ? 0.98 : 1.0)
+    }
+}
+
+#Preview {
+    Button("Create invoice") {}
+        .buttonStyle(MainButton())
+        .padding(16)
+}
